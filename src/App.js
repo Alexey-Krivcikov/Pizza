371638d04a7510ce310de18f7ac3162be8241f61
Alexey Krivchikov <krivchikovalexei@yandex.ru>
2023-08-13 19:13:13 +0300

    feat: прикрутил ReactRouter добавил CSS module для NotFoundBlock

--- src/App.js
+++ src/App.js
@@ -1,43 +1,27 @@
 import './scss/app.scss';
 import React from 'react';
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort'
-import PizzaBlock from './components/PizzaPlock';
-import Skeleton from './components/PizzaPlock/Skeleton';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
-  const [items, setItems] = React.useState([])
-  const [isLoading, setIsLoading] = React.useState(true);
 
-  React.useEffect(() => {
-    fetch('https://64d5e9f3754d3e0f13615048.mockapi.io/items')
-      .then(res => res.json())
-      .then(arr => {
-        setItems(arr)
-        setIsLoading(false)
-      })
-  }, [])
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {
-              isLoading
-                ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-                : items.map(obj => (
-                  <PizzaBlock key={obj.id} {...obj} />
-                ))}
-          </div>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='*' element={<NotFound />} />
+            <Route path='/cart' element={
+              <Cart />
+            } />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => {
+  return (
+    <>
+      <h1>Korzina</h1>
+    </>
+  );
+};
+
+export default Cart;
