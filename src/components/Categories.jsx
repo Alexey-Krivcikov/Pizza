82b2dda417533d1fec7cc576e98a8c60ca6601a5
Alexey Krivchikov <krivchikovalexei@yandex.ru>
2023-08-17 18:15:26 +0300

    feat: сделать фильтрацию и сортировку пицц

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-function Categories() {
-  const [activeIndex, setActiveIndex] = React.useState(0);
-
+function Categories({ value, onChangeCategory }) {
   const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
   return (
@@ -11,8 +9,8 @@ function Categories() {
         {categories.map((category, index) => (
           <li
             key={index}
-            onClick={() => setActiveIndex(index)}
-            className={activeIndex === index ? 'active' : ''}>
+            onClick={() => onChangeCategory(index)}
+            className={value === index ? 'active' : ''}>
             {category}
           </li>
         ))}
